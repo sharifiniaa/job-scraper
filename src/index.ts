431c8c5a619c538da1f749032e040915e97774d7
hossein.sha <hossein.sh@780.team>
2023-04-19 15:46:39 +0330

    refactor(struce-modules): modulize the scrapper funtions

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ require('dotenv').config();
 import {elementGetter} from './modules/scraper/elements';
 import {createDriver} from './modules/driver';
 import {By, until} from 'selenium-webdriver';
-import prisma from './modules/db';
 import {TJob} from './types';
-import {parsPath} from './modules/scraper/parsPath';
+import {filterKeyword} from 'modules/scraper/filterKeywords';
+import {checkAndSaveJobs} from 'modules/scraper/saveJobs';
 
 export const scraper = async (location: string) => {
   const driver = await createDriver();
@@ -14,16 +14,16 @@ export const scraper = async (location: string) => {
       `https://www.linkedin.com/jobs/search?keywords=React&location=${location}&f_TPR=r86400&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0`,
     );
 
-    // for (let i = 0; i < 4; i++) {
-    //   // Scroll to the bottom of the page
-    //   await driver.executeScript('window.scrollTo(0, document.body.scrollHeight);');
+    for (let i = 0; i < 4; i++) {
+      // Scroll to the bottom of the page
+      await driver.executeScript('window.scrollTo(0, document.body.scrollHeight);');
 
-    //   // Wait for new content to load
-    //   await driver.wait(until.elementLocated(By.css('ul.jobs-search__results-list>li')));
+      // Wait for new content to load
+      await driver.wait(until.elementLocated(By.css('ul.jobs-search__results-list>li')));
 
-    //   // Wait for some additional time to allow the page to fully render
-    //   await driver.sleep(5000);
-    // }
+      // Wait for some additional time to allow the page to fully render
+      await driver.sleep(5000);
+    }
 
     // Get job listings
     console.log('before listing');
@@ -38,10 +38,9 @@ export const scraper = async (location: string) => {
       const location = await elementGetter({el, selector: 'span.job-search-card__location'});
       const time = await elementGetter({el, selector: 'time'});
       const link = await elementGetter({el, selector: 'a.base-card__full-link', method: 'attribute', attr: 'href'});
-      const visa = !!title?.includes('visa sponsorship');
-      jobs.push({title, company, location, time, link, visa});
+      jobs.push({title, company, location, time, link, visa: false});
     }
-    const filteredJobs = await filterJobs(jobs);
+    const filteredJobs = await filterKeyword(jobs);
     await checkAndSaveJobs(filteredJobs);
   } catch (err) {
     console.log(err);
@@ -50,77 +49,5 @@ export const scraper = async (location: string) => {
     await driver.quit();
   }
 };
-
-async function filterJobs(jobs: TJob[]): Promise<TJob[]> {
-  const driver = await createDriver();
-  try {
-    const filteredJobs: TJob[] = [];
-    for (const job of jobs) {
-      console.log('finding keywords ...');
-      await driver.get(job.link);
-      const showMoreBtn = await driver.findElement(By.className('show-more-less-html__button--more'));
-      await showMoreBtn.click();
-      await driver.sleep(3000);
-      const element = await driver.findElement(By.className('core-section-container'));
-      const text = await element.getText();
-      await driver.sleep(1000);
-      job.visa = text.toLocaleLowerCase().includes('visa sponsorship');
-      filteredJobs.push(job);
-      const handles = await driver.getAllWindowHandles();
-      for (let i = 1; i < handles.length; i++) {
-        await driver.switchTo().window(handles[i]);
-        await driver.close();
-      }
-      await driver.switchTo().window(handles[0]);
-    }
-    return filteredJobs;
-  } catch (err) {
-    console.log(err);
-    await driver.quit();
-    throw new Error('filtered broken');
-  } finally {
-    await driver.quit();
-  }
-}
-
-
-async function checkAndSaveJobs(jobs: TJob[]) {
-  let count = 0;
-  try {
-    await Promise.all(
-      jobs.map(async job => {
-        const job_name: string = parsPath(job.link);
-        const existingJob = await prisma.job.findUnique({
-          where: {
-            job_name,
-          },
-        });
-
-        if (!existingJob) {
-          console.log('its unique:', job.title, job.link);
-          count = count + 1;
-          await prisma.job.create({
-            data: {
-              title: job.title,
-              company: job.company,
-              location: job.location,
-              time: job.time,
-              link: job.link,
-              job_name: job_name,
-            },
-          });
-        }
-      }),
-    );
-
-    console.log('Jobs saved to database successfully!');
-  } catch (e: any) {
-    console.error(`Error saving jobs to database: ${e.message}`);
-  } finally {
-    console.log(jobs.length, 'jobs found');
-    console.log('new items', count);
-    await prisma.$disconnect();
-  }
-}
 
 scraper('United kingdom');
