508386ec3acd6196008034e71d01698cf36ee471
hossein.sha <hossein.sh@780.team>
2023-04-26 14:49:49 +0330

    feat(title-filters): add title filters to env variable

--- src/modules/scraper/isExcludedByTitle.ts
+++ src/modules/scraper/isExcludedByTitle.ts
@@ -1,4 +1,5 @@
 export function isExcludedByTitle(title: string): boolean {
-  const keywords = ['junior', 'no visa sponsorship', 'no visasponsorship', 'no visa', 'entry level', 'remote'];
+  // const keywords = ['junior', 'no visa sponsorship', 'no visasponsorship', 'no visa', 'entry level', 'remote'];
+  const keywords = (process.env.TITLE_FILTER_KEYWORDS as string).split(',').map(el => el.trim());
   return !keywords.some(keyword => title.includes(keyword));
 }
